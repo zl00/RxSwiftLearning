0226088e66eacc1c99cc4ff16817cd2c43448c5c
Linda Zhong <wm80098@worksmobile.com>
2018-05-31 10:53:55 +0800

    Initial Commit

---
+++ RxSwiftLearnig/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RxSwiftLearnig
+//
+//  Created by Linda Zhong on 5/31/18.
+//  Copyright © 2018 Null. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ RxSwiftLearnig/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RxSwiftLearnig
+//
+//  Created by Linda Zhong on 5/31/18.
+//  Copyright © 2018 Null. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
